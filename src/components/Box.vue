822872c788689a1b8c502692d360e871daef588c
Steve Yu <soundedrain@gmail.com>
2019-08-28 04:05:07 +0800

    Updates

---
+++ src/components/Box.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="box">
+    <article class="media">
+      <div class="media-left">
+        <figure class="image is-64x64">
+          <b-icon
+            pack="fas"
+            icon="coffee"
+            size="is-medium">
+          </b-icon>
+        </figure>
+      </div>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+            <br>
+            <span v-html="talk.title"></span>
+          </p>
+        </div>
+        <!-- <nav class="level is-mobile">
+          <div class="level-left">
+            <a class="level-item" aria-label="reply">
+              <span class="icon is-small">
+                <i class="fas fa-reply" aria-hidden="true"></i>
+              </span>
+            </a>
+            <a class="level-item" aria-label="retweet">
+              <span class="icon is-small">
+                <i class="fas fa-retweet" aria-hidden="true"></i>
+              </span>
+            </a>
+            <a class="level-item" aria-label="like">
+              <span class="icon is-small">
+                <i class="fas fa-heart" aria-hidden="true"></i>
+              </span>
+            </a>
+          </div>
+        </nav> -->
+      </div>
+    </article>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Box',
+  props: ['talk'],
+  computed: {
+    speaker() {
+      return this.talk.speaker[0] || null
+    },
+
+    hasSpeaker() {
+      return !!this.talk.speaker.length
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,103 @@
+<template>
+  <Layout>
+    <Table :talks="realTalks" />
+    <!-- All other tile elements -->
+<!--     <template v-for="talk in realTalks">
+      <div v-if="Object.values(talk)[0] instanceof Array" class="columns" :key="Object.keys(talk)[0]">
+        <div v-for="session in Object.values(talk)[0]" class="column">
+          <Card class="is-primary" :talk="session" />
+        </div>
+      </div>
+      <div v-else class="columns" :key="Object.keys(talk)[0]">
+        <div class="column">
+          <Box :talk="Object.values(talk)[0]" />
+        </div>
+      </div>
+    </template> -->
+  </Layout>
+</template>
+
+<script>
+// import Card from '~/components/Card.vue'
+// import Box from '~/components/Box.vue'
+import Table from '~/components/Table.vue'
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  },
+  computed: {
+    realTalks() {
+      let result = []
+      this.$page.talks.edges.forEach(v => {
+        let item = v.node
+        if(item.sessionType === 'break' || item.sessionType === 'keynote') {
+          result.push({[item.sort]: item})
+        } else if(item.sessionType === 'session') {
+          // find exising node
+          let session = result.find(k => {
+            return Object.keys(k)[0] == item.sort
+          })
+
+          if(!session) {
+            session = {[item.sort]: {
+              title: '議程',
+              items: [],
+              sessionType: 'session',
+              date: item.date,
+              startDate: item.startDate,
+              startDateText: item.startDateText,
+              endDate: item.endDate,
+              endDateText: item.endDateText,
+              sort: item.sort
+            }}
+            result.push(session)
+          }
+          session[item.sort]['items'].push(item)
+        }
+      })
+
+      return result
+    }
+  },
+  components: {
+    Table
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
+
+
+<page-query>
+query Talks {
+  talks: allTalks(sort: [{ by: "sort", order: ASC }, { by: "track", order: ASC}]) {
+    edges {
+      node {
+        id
+        title
+        classroom
+        track
+        date
+        startDate
+        startDateText
+        endDate
+        endDateText
+        sessionType
+        summary
+        language
+        sort
+        path
+        speaker {
+          name
+          avatar
+          company
+        }
+      }
+    }
+  }
+}
+</page-query>
